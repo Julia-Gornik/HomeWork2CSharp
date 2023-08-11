ac8cb8433f141ec560b1d77c9b1b57037ed5c1d8
Julia-Gornik <Stepanovaulia131@gmail.com>
2023-08-11 12:30:21 +0300

    Task 13

--- Task_013/Program.cs
+++ Task_013/Program.cs
@@ -4,3 +4,34 @@
 32679 -> 6
 */
 
+int Promt(string massage)
+{
+    Console.Write(massage);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int GetThirdRank(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 100 && number > -100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+int number = Promt("Введите число > -999      ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetThirdRank(number));
+}
---
+++ Task_015/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+int Promt(string massage)
+{
+    Console.Write(massage);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+bool Weekend(int weekday)
+{
+    if (weekday > 5)
+    {
+        return true;
+    }
+    return false;
+}
+bool ValidateWeekday(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Такого дня недели нет");
+    return false;
+}
+
+int weekday = Promt("Введите день недели цифрой ");
+if (ValidateWeekday(weekday))
+{
+    if (Weekend(weekday))
+    {
+        Console.WriteLine("Это выходной");
+    }
+    else
+    {
+        Console.WriteLine("Это не выходной");
+    }
+}
